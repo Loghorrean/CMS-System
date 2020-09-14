c761456defd91b31fe29dc584e5845e8dc709d86
Loghorrean <loghorrean74@gmail.com>
2020-09-15 01:01:29 +0300

    Added pagination

--- index.php
+++ index.php
@@ -6,9 +6,32 @@ checkUsersCookie($pdo);
 <!-- Navigation -->
 <?php
 require_once("includes/navigation.php");
+if (isset($_GET["page"])) {
+    if (!checkId($_GET["page"])) {
+        header("Location: ./");
+        return;
+    }
+    else {
+        $page = $_GET["page"];
+    }
+    if ($page == "" || $page == 1) {
+        $page_1 = 0;
+    }
+    else {
+        $page_1 = ($page * 5) - 5;
+    }
+}
+else {
+    $page_1 = 0;
+}
+$query = $pdo->prepare("SELECT count(*) as 'count' from posts");
+$count_posts = $query->execute();
+$count_posts = $query->fetch(PDO::FETCH_LAZY)["count"];
+$count_posts = ceil($count_posts / 5);
 $sql = "SELECT users.username as 'username', posts.* from posts ";
-$sql .= "left join users on users.user_id = posts.post_author_id where post_status = 'published'";
+$sql .= "left join users on users.user_id = posts.post_author_id where post_status = 'published' LIMIT :page, 5";
 $query = $pdo->prepare($sql);
+$query->bindParam(":page", $page_1, PDO::PARAM_INT);
 $query->execute();
 ?>
 <!-- Page Content -->
@@ -38,6 +61,17 @@ $query->execute();
                 <li class="previous">
                     <a href="#">&larr; Older</a>
                 </li>
+                <?php
+                for($i = 1; $i <= $count_posts; $i++) {
+                    if (isset($_GET["page"]) && $i == $_GET["page"]) {
+                        $current_page = 
+                    }
+                    else {
+                        $current_page =
+                    }
+                    echo "<li><a href='index.php?page={$i}'>{$i}</a></li>";
+                }
+                ?>
                 <li class="next">
                     <a href="#">Newer &rarr;</a>
                 </li>
@@ -51,7 +85,6 @@ $query->execute();
         ?>
     </div>
     <!-- /.row -->
-
     <hr>
     <?php
     require_once ("includes/footer.php");
